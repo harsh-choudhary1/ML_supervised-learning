5873b869077b1c112af91440881fc7411127e8c0
harsh-choudhary1 <160221948+harsh-choudhary1@users.noreply.github.com>
2024-12-27 10:40:39 +0530

    Delete irisdataset.py

--- irisdataset.py
+++
@@ -1,9 +0,0 @@
-import matplotlib as plt
-import numpy as np 
-from sklearn import datasets, linear_model
-
-datas = datasets.load_iris()
-# print(data_s.keys())
-#output of data_s.keys# keys = = dict_keys(['data', 'target', 'frame', 'target_names', 'DESCR', 'feature_names', 'filename', 'data_module'])
-datas_x = iris.data[:, np.newaxis,2]
-print(datas_x)
